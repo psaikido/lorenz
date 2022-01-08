f231f6ff156cb224333cf5c338c7a11d9a087e8f
Hugh Carroll <hughie@carrollonline.co.uk>
2022-01-08 21:17:51 +0000

    Translate into python.

---
+++ python/lorenz.py
@@ -0,0 +1,129 @@
+#! /usr/bin/python
+
+baudot = {
+    " " : "00100",
+    "." : "00010",
+    "!" : "11111",
+    "?" : "00000",
+    "-" : "01000",
+    "&" : "11011",
+    "A" : "00011",
+    "B" : "11001",
+    "C" : "01110",
+    "D" : "01001",
+    "E" : "00001",
+    "F" : "01101",
+    "G" : "11010",
+    "H" : "10100",
+    "I" : "00110",
+    "J" : "01011",
+    "K" : "01111",
+    "L" : "10010",
+    "M" : "11100",
+    "N" : "01100",
+    "O" : "11000",
+    "P" : "10110",
+    "Q" : "10111",
+    "R" : "01010",
+    "S" : "00101",
+    "T" : "10000",
+    "U" : "00111",
+    "V" : "11110",
+    "W" : "10011",
+    "X" : "11101",
+    "Y" : "10101",
+    "Z" : "10001",
+}
+
+alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+
+chiOne = 41
+chiTwo = 31
+
+def code(plainText, settings):
+    plainBytes = plainToBytes(plainText)
+    keyBytes = plainToBytes(makeKeyStream(settings, len(plainText)))
+    cipherBytes = bitwiseEncode(plainBytes, keyBytes)
+    cipherText = bytesToPlain(cipherBytes)
+
+    return cipherText
+
+
+def xorBits(i, j):
+    if i == j:
+        return 0
+    else:
+        return 1
+
+def xorBytes(a, b):
+    tmpStr = ''
+
+    for x in range(len(a)):
+        tmpStr += str(xorBits(a[x], b[x]))
+
+    return tmpStr
+
+def bitwiseEncode(plainBytes, keyBytes):
+    retA = []
+
+    for x in range(len(plainBytes)):
+        retA.append(xorBytes(plainBytes[x], keyBytes[x]))
+        
+    return retA
+
+def makeChiArray(upperLimit, startingPos):
+    chiAr = []
+    y = startingPos
+
+    for x in range(upperLimit):
+        chiAr.append([
+            alphabet[y],
+            baudot[alphabet[y]]
+            ])
+
+        if y >= 25:
+            y = 0
+        else:
+            y += 1
+
+    return chiAr
+
+def makeKeyStream(settings, keyLength):
+    retStr = ""
+    y = z = 0
+
+    chiOneAr = makeChiArray(chiOne, settings[0])
+    chiTwoAr = makeChiArray(chiTwo, settings[1])
+
+    for x in range(keyLength):
+        if y >= chiOne:
+            y = 0
+        if z >= chiTwo:
+            z = 0
+        chiOneByte = chiOneAr[y][1]
+        chiTwoByte = chiTwoAr[z][1]
+        xorProduct = xorBytes(list(chiOneByte), list(chiTwoByte))
+        resultantLtr = list(baudot.keys())[list(baudot.values()).index(xorProduct)]
+        retStr += resultantLtr
+        
+        y += 1
+        z += 1
+
+    return retStr
+
+def bytesToPlain(cipherBytes):
+    retStr = ''
+
+    for fiveb in cipherBytes:
+        retStr += list(baudot.keys())[list(baudot.values()).index(fiveb)]
+
+    return retStr
+
+def plainToBytes(plain):
+    bits = []
+
+    for c in list(plain):
+        bits.append(baudot[c.upper()])
+
+    return bits
+
---
+++ python/test_lorenz.py
@@ -0,0 +1,97 @@
+#! /usr/bin/python
+
+def test_xorBits():
+    import lorenz
+
+    assert lorenz.xorBits('0', '0') == 0
+    assert lorenz.xorBits('0', '1') == 1
+    assert lorenz.xorBits('1', '0') == 1
+    assert lorenz.xorBits('', '0') == 1
+
+def test_xorBytes():
+    import lorenz
+
+    assert lorenz.xorBytes([1,0,0,0,0], [0,0,0,0,1]) == '10001'
+    assert lorenz.xorBytes([0,1,0,1,0], [1,0,1,0,1]) == '11111'
+    assert lorenz.xorBytes([1,1,1,1,1], [1,0,1,0,1]) == '01010'
+
+def test_bitwiseEncode():
+    import lorenz
+
+    assert lorenz.bitwiseEncode(['10000'],['10011']) == ['00011']
+    assert lorenz.bitwiseEncode(['10100'],['10011']) == ['00111']
+    assert lorenz.bitwiseEncode(['10111'],['10110']) == ['00001']
+
+def test_makeChiArray():
+    import lorenz
+
+    assert lorenz.makeChiArray(3,0) == [
+            ['A', '00011'],
+            ['B', '11001'],
+            ['C', '01110']
+        ]
+def test_makeKeyStream():
+    import lorenz
+
+    key0 = 'N!ROAWYJZ&'
+    key1 = 'N!ROAWYJZ&HQARR&LWUF&AGJ.VN!ROAKSSLE&XE..!SIWZPNTMSQNJISR!HW'
+    key2 = 'N!ROAWYJZ&HQARR&LWUF&AGJ.VN!ROAKSSLE&XE..!SIWZPNTMSQNJISR!HWVTINP-FTK &BM!DBWV GKMMVC!VUEQ-PMDX&KBA XXRYPGA!YP!PU! EYMKDX&KQ&O.HHXUIYIGUQ JIXITHPFLKT &O.HHEQ-PMYFYNF-.C!IOVXOOBM!- QTE UX- LGQU-NQCLF X'
+    key3 = '! EYMKDX&K'
+
+    assert lorenz.makeKeyStream([1,24], len(key0)) == key0
+    assert lorenz.makeKeyStream([1,24], len(key1)) == key1
+    assert lorenz.makeKeyStream([1,24], len(key2)) == key2
+    assert lorenz.makeKeyStream([18,6], len(key3)) == key3
+
+def test_bytesToPlain():
+    import lorenz
+
+    assert lorenz.bytesToPlain(['00001']) == "E"
+    assert lorenz.bytesToPlain(['00100']) == " "
+    assert lorenz.bytesToPlain(['00010']) == "."
+    #assert lorenz.bytesToPlain(['4']) == "ValueError"
+    #assert lorenz.bytesToPlain(['elf']) == ""
+
+def test_plainToBytes():
+    import lorenz
+
+    assert lorenz.plainToBytes('E') == ['00001']
+    assert lorenz.plainToBytes(' ') == ['00100']
+    assert lorenz.plainToBytes('.') == ['00010']
+    assert lorenz.plainToBytes('') == []
+    #assert lorenz.plainToBytes('*') == 'Undefined index: *'
+    #assert lorenz.plainToBytes('[') == 'Undefined index: ['
+    #assert lorenz.plainToBytes('3') == 'Undefined offset: 3'
+
+def test_code():
+    import lorenz
+
+    text0 = "ABC"
+    cipherText0 = "&JD"
+
+    text1 = "abc"
+    cipherText1 = "HQM"
+
+    text2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cipherText2 = "XDHYBIFJAFMAR?-RLXD&E&B.E."
+
+    text3 = "OM AH HUM BENZA GURU PEMA SIDDHI HUM"
+    cipherText3 = "DUTHQCVMCQVNQLBKOBIOCC.KPKHXXVQ!QRAI"
+
+    text4 = "THESE VIOLENT DELIGHTS HAVE VIOLENT ENDS AND IN THEIR TRIUMPH DIE LIKE FIRE AND POWDER WHICH AS THEY KISS CONSUME"
+    cipherText4 = "O.XTNZLSHMVBL&N.HKIRVGGW.DDL.WYDCYW?MZATLUZUHL.SF&HZWDV..YIEWNJLYBY?GUVRZCR.VRBT?Y!O?JXGE-ROYU.B ?XNDU JG.PIZX!SX"
+
+    assert lorenz.code(text0, [9,22]) == cipherText0
+    assert lorenz.code(text0, [9,22]) == cipherText0
+    assert lorenz.code(cipherText0, [9,22]) == text0
+    assert lorenz.code(cipherText0, [8,22]) == "CXW"
+    assert lorenz.code(cipherText0, [9,23]) == "FZR"
+
+    assert lorenz.code(text1, [5,6]) == cipherText1
+    assert lorenz.code(cipherText1, [5,6]) == text1.upper()
+    assert lorenz.code(text2, [20,1]) == cipherText2
+    assert lorenz.code(cipherText2, [20,1]) == text2
+    assert lorenz.code(text3, [6,9]) == cipherText3
+    assert lorenz.code(cipherText3, [6,9]) == text3
+    assert lorenz.code(text4, [9,0]) == cipherText4
+    assert lorenz.code(cipherText4, [9,0]) == text4
